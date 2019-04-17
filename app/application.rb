69ef60fe34a62bfdf32a0df52ed6fa3986f29160
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-17 00:22:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
   
   @@items = [Item.new("Milk", 4.99), Item.new("Cheese", 4.11)]
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request(env)
@@ -8,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       
-      if item
+      if item=@@items.find
       
   end
 end
